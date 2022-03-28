213978bee5be0a0b30a1c78ab3dcbfbe48dce2a1
Medsar2000 i7 <Mehdi.sarhadi@gmail.com>
2022-03-28 15:01:19 +0430

    buttons added
    buttons added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Text playerNameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNameText.text = SaveManager.Instance.playerName;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
